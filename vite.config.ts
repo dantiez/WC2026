e36271f2c9758a95f738e174bcd3a539046847f7
dantiez <danmap987@gmail.com>
2026-05-25 17:48:50 +0700

    feat: update manualChunks configuration to use 'exceljs' instead of 'xlsx'

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(() => {
       rollupOptions: {
         output: {
           manualChunks: {
-            xlsx: ['xlsx'],
+            exceljs: ['exceljs'],
             'lucide-react': ['lucide-react'],
           },
         },
